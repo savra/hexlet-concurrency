621de3763b0928c3928852dc5bee267e941cd3eb
Sergey Savrukhin <savra.sv@yandex.ru>
2022-11-04 17:43:08 +0300

    init commit

---
+++ src/main/java/com/hvdbs/hexletconcurrency/HexletConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.hvdbs.hexletconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexletConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexletConcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hvdbs/hexletconcurrency/HexletConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvdbs.hexletconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexletConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
